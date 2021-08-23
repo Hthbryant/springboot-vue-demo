ea515d59c0ff1a9d84cc49890250af363c267fd0
huang <1635618490@qq.com>
2021-08-24 02:09:13 +0800

    添加日志打印，问题尚未解决

--- springboot/src/main/java/com/example/controller/UserController.java
+++ springboot/src/main/java/com/example/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.common.dto.UserRequestDTO;
 import com.example.common.dto.UserResponseDTO;
 import com.example.common.enums.ErrorEnum;
 import com.example.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -24,13 +26,13 @@ public class UserController {
     public Result queryUserList(HttpServletRequest httpReq,
                                                  HttpServletResponse httpResp,
                                                  @RequestBody UserRequestDTO requestDTO) {
+        log.info("query userList request:{}",requestDTO);
         Result SystemErrorResult = new Result(ErrorEnum.SYSTEM_ERROR);
         try {
             UserResponseDTO userResponseDTO = userService.queryUserList();
-            Result result = new Result(userResponseDTO);
-            return result;
+            return new Result(userResponseDTO);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("query userList error:",e);
             return SystemErrorResult;
         }
     }
@@ -39,13 +41,13 @@ public class UserController {
     public Result queryUserInfo(HttpServletRequest httpReq,
                                                  HttpServletResponse httpResp,
                                                  @RequestBody UserRequestDTO requestDTO) {
+        log.info("===query userInfo request:{}",requestDTO);
         Result SystemErrorResult = new Result(ErrorEnum.SYSTEM_ERROR);
         try {
             UserResponseDTO userResponseDTO = userService.querUserInfo(requestDTO);
-            Result result = new Result(userResponseDTO);
-            return result;
+            return new Result(userResponseDTO);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("===query userInfo error:",e);
             return SystemErrorResult;
         }
     }
@@ -54,14 +56,13 @@ public class UserController {
     public Result addUser(HttpServletRequest httpReq,
                                                  HttpServletResponse httpResp,
                                                  @RequestBody UserRequestDTO requestDTO) {
-        System.out.println("requestDTO:"+requestDTO);
+        log.info("===add user request:{}",requestDTO);
         Result SystemErrorResult = new Result(ErrorEnum.SYSTEM_ERROR);
         try {
             UserResponseDTO userResponseDTO = userService.addUser(requestDTO);
-            Result result = new Result(userResponseDTO);
-            return result;
+            return new Result(userResponseDTO);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("===add user error:",e);
             return SystemErrorResult;
         }
     }
@@ -70,13 +71,13 @@ public class UserController {
     public Result updateUser(HttpServletRequest httpReq,
                                                  HttpServletResponse httpResp,
                                                  @RequestBody UserRequestDTO requestDTO) {
+        log.info("update userInfo request:{}",requestDTO);
         Result SystemErrorResult = new Result(ErrorEnum.SYSTEM_ERROR);
         try {
             UserResponseDTO userResponseDTO = userService.updateUser(requestDTO);
-            Result result = new Result(userResponseDTO);
-            return result;
+            return new Result(userResponseDTO);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("update userInfo error:",e);
             return SystemErrorResult;
         }
     }
@@ -85,13 +86,13 @@ public class UserController {
     public Result delete(HttpServletRequest httpReq,
                                           HttpServletResponse httpResp,
                                           @RequestBody UserRequestDTO requestDTO) {
+        log.info("delete user request:{}",requestDTO);
         Result SystemErrorResult = new Result(ErrorEnum.SYSTEM_ERROR);
         try {
             UserResponseDTO userResponseDTO = userService.deleteUser(requestDTO);
-            Result result = new Result(userResponseDTO);
-            return result;
+            return new Result(userResponseDTO);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("delete user error:",e);
             return SystemErrorResult;
         }
     }
